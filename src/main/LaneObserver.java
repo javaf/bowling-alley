8d992c55f8759f985f1e6ea2ca3258eda4b2ab1f
Subhajit Sahu <wolfram77@gmail.com>
2019-02-04 04:30:56 +0530

    delete lane files

--- src/main/LaneObserver.java
+++
@@ -1,5 +0,0 @@
-package main;
-
-public interface LaneObserver {
-  public void receiveLaneEvent(LaneEvent le);
-};
--- src/main/PinsetterObserver.java
+++
@@ -1,5 +0,0 @@
-package main;
-
-public interface PinsetterObserver {
-  public void receivePinsetterEvent(PinsetterEvent pe);
-};
