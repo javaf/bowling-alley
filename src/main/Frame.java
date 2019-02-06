492b95fa98297ffbe0ee216524a9a52dbc92e587
Subhajit Sahu <wolfram77@gmail.com>
2019-02-06 15:10:11 +0530

    :mushroom: frame does not need turn

--- src/main/Frame.java
+++ src/main/Frame.java
@@ -13,16 +13,16 @@ public class Frame extends ArrayList<Roll> {
   public int score() {
     return score;
   }
-  
-  public Roll last() {
-    return isEmpty()? null : get(size() - 1);
-  }
-  
+
   public boolean complete(boolean tenth) {
     int size = size();
     if(!tenth) return size==0? false : (size>=2 || last().full());
     boolean bonus = (size>0 && get(0).full()) || (size>1 && get(1).full());
     return bonus? size>=3 : size>=2;
+  }
+  
+  public Roll last() {
+    return isEmpty()? null : get(size() - 1);
   }
 
   @Override
@@ -42,7 +42,7 @@ public class Frame extends ArrayList<Roll> {
   
   public StringBuilder stringify(StringBuilder out, String pad) {
     out.append(pad).append("[Frame]\n");
-    out.append(pad).append("score: ").append(score).append('\n');
+    out.append(pad).append("score: ").append(score()).append('\n');
     for(int i=0; i<size(); i++) {
       out.append(pad).append("roll[").append(i).append("]:\n");
       get(i).stringify(out, pad+"  ").append('\n');
